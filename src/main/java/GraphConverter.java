3f25f57c0b55a3a7f23833e1ebe58d093b16849c
PhaniGaddipati <phanigaddipati@gmail.com>
2016-07-13 15:59:48 -0400

    write the json

--- src/main/java/GraphConverter.java
+++ src/main/java/GraphConverter.java
@@ -1,8 +1,19 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GraphConverter {
     static String graph_file;
@@ -23,5 +34,51 @@ public class GraphConverter {
         } catch (IOException e) {
             System.out.println("INVALID FILE");
         }
+
+        Map<Vertex, JsonObject> verts = new HashMap<Vertex, JsonObject>();
+        for (Vertex v : graph.getVertices()) {
+            verts.put(v, vertexToJsonObject(v));
+        }
+        for (Edge e : graph.getEdges()) {
+            JsonObject up = verts.get(e.getVertex(Direction.OUT));
+            JsonObject down = verts.get(e.getVertex(Direction.IN));
+
+            JsonArray questionIds = up.getAsJsonArray("next_question");
+            questionIds.add(down.get("id"));
+            up.add("next_question", questionIds);
+
+            JsonArray optionText = up.getAsJsonArray("options");
+            optionText.add(e.getProperty("option").toString());
+            up.add("options", optionText);
+        }
+
+        JsonArray all = new JsonArray();
+        for (Vertex v : verts.keySet()) {
+            all.add(verts.get(v));
+        }
+
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(graph_file.replace(".graphml", ".json")));
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            writer.print(gson.toJson(all));
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static JsonObject vertexToJsonObject(Vertex v) {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("id", (int) (Math.random() * 999999999) + "");
+        obj.addProperty("question", v.getProperty("question") == null ? "" : v.getProperty("question").toString());
+        obj.addProperty("details", v.getProperty("details") == null ? "" : v.getProperty("details").toString());
+        if (v.getPropertyKeys().contains("imageURL")) {
+            obj.addProperty("image", v.getProperty("imageURL").toString());
+        }
+        obj.add("attachment", null);
+        obj.add("options", new JsonArray());
+        obj.add("next_question", new JsonArray());
+        return obj;
     }
 }
