66947889c0f1ef423c317d5b3e0bbd28effa8aec
Doran W <doranwalsten@Dorans-MacBook-Air.local>
2016-07-20 14:39:27 -0400

    restructuring on Doran side

--- src/main/java/GraphConverter.java
+++ src/main/java/GraphConverter.java
@@ -1,3 +1,4 @@
+package main.java;
 import com.google.gson.*;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
@@ -5,7 +6,6 @@ import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
